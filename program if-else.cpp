68758c94076819cca7931d26a23da655e084a141
Adlyaptri <117509663+Adlyaptri@users.noreply.github.com>
2022-11-15 12:09:13 +0700

    Add files via upload

---
+++ program if-else.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cout<<"============================\n";
+	cout<<"Nama: Adelya Destriana Putri\n";
+	cout<<"NIM: 22343033\n";
+	cout<<"Membuat Program if else\n";
+	cout<<"============================\n";
+	string kata;
+	cout<<"Masukan kata =  BELAJAR KODING"<<endl;
+	cin>>kata;
+	
+	if (kata=="BELAJAR KODING"){
+		cout<<"Kata yang dimasukan sesuai"<<endl;
+	}else {
+		cout<<"Kata yang dimasukan tidak sesuai"<<endl;
+	}
+
+getch();
+}
